706df36e32d2c49523c3b60ee5f8add7907e1e4e
visualstudio <visualstudio@122-PC15>
2019-02-22 19:17:40 -0500

    7:15 Commit

--- CSD-3354-1-Aslaug/Program.cs
+++ CSD-3354-1-Aslaug/Program.cs
@@ -13,4 +13,71 @@ namespace CSD_3354_1_Aslaug
 
         }
     }
+
+
+
+class JournalEntry
+
+    {
+
+        public JournalEntry(string note, int dist)
+        {
+
+            villageName = note; distanceTraveled = dist;
+
+            HowFarToGetBack = distanceTraveled;
+
+        }
+
+        public int HowFarToGetBack = 0;
+
+        private string villageName;
+
+        private int distanceTraveled;
+
+        public int getDistanceWalked() { return distanceTraveled; }
+
+        public string getVillageName() { return villageName; }
+
+    }
+
+
+
+    class Hugi
+
+    {
+
+        private static JournalEntry je;
+
+        public static bool FoundAstrilde = false;
+
+
+
+        public static List<JournalEntry> HugiJournal = new List<JournalEntry>();
+
+
+
+        public static int CalculateDistanceWalked()
+
+        {
+
+            int DistanceWalked = 0;
+
+            // walk over the List and add the distances
+
+            foreach (var je in HugiJournal)
+
+            {
+
+                Console.WriteLine(" {0}  --   {1} *** --- {2} ", je.getDistanceWalked(), je.getVillageName(), je.HowFarToGetBack);
+
+                DistanceWalked += je.getDistanceWalked() + je.HowFarToGetBack;
+
+            }
+
+            return DistanceWalked;
+
+        }
+
+    }
 }
